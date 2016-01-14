ee12678b1c7445e3115d7d8a29b36038ddd19c49
Jeremy Warner <jeremywrnr@gmail.com>
2016-01-14 18:12:49 -0500

    fixing filenames

--- lib/modname/modder.rb
+++ lib/modname/modder.rb
@@ -57,7 +57,7 @@ class << Modder
   # double check transformations
   def confirm?
     print "Are these changes ok? [yN] "
-    (gets.chomp!).downcase[0] == "y"
+    ($stdin.gets.chomp!).downcase[0] == "y"
   end
 
   # return a list of files to examine
