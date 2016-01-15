b6111c114ed153d03fa27e8c1646cbfed4a56f61
Jeremy Warner <jeremywrnr@gmail.com>
2016-01-14 22:28:28 -0500

    retouch banners

--- lib/modname/banner.rb
+++ lib/modname/banner.rb
@@ -8,7 +8,6 @@ Modname::HelpBanner = <<-HELP
      -f #{'|'.cyan} force run; don't pre-check
      -r #{'|'.cyan} run modname recursively
      -h #{'|'.cyan} show more help, examples
-
 HELP
 
 
@@ -25,14 +24,14 @@ Modname::VHelpBanner = <<-VHELP
     nil => move all extensions to lower case
 
 #{'examples'.cyan}
-  file names
+  file names (f)
     modname f hello => deletes 'hello' from all filenames
     modname f hello byebye => replace 'hello' with byebye
 
-  extensions
+  extensions (e)
     modname e txt md => move all txt files to markdown
     modname e mov mp4 => move all mov files to mp4
 
-note: <required> [optional]
+#{'Note:'.cyan} <required> [optional]
 VHELP
 
